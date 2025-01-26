3a776be479acdbaa6887ffd5c0cfa6ccd82157bd
Greg Papadopoulos <venscarlet@MacBookAir.home>
2025-01-26 17:05:58 +0200

    Integrated react with laravel

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,25 +1,8 @@
-import '../css/app.css';
-import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot } from 'react-dom/client';
+function App() {
+    return <h1>Hello, React with Laravel!</h1>;
+}
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.jsx`,
-            import.meta.glob('./Pages/**/*.jsx'),
-        ),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
-
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+ReactDOM.render(<App />, document.getElementById('app'));
